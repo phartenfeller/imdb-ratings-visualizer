d4357e490e532e630731b1de652cb296b2aa5dec
phartenfeller <pharti@hotmail.de>
2019-03-06 20:55:18 +0100

    Updates

---
+++ precache-manifest.4038e4cf8c914143952af75e65aa674c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd0d741cc8172238de9e",
+    "url": "/static/css/main.ce4746ea.chunk.css"
+  },
+  {
+    "revision": "cd0d741cc8172238de9e",
+    "url": "/static/js/main.cd0d741c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bb2ca53e6213ff48e6c7",
+    "url": "/static/js/2.bb2ca53e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0169dc8b4fa10029e19dd9482ec8f0db",
+    "url": "/index.html"
+  }
+];
