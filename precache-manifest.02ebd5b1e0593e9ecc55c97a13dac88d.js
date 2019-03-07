ecefd0e16d6c4bb6d1728b3383c52072c4b6b4d0
phartenfeller <pharti@hotmail.de>
2019-03-07 12:57:01 +0100

    Updates

---
+++ precache-manifest.02ebd5b1e0593e9ecc55c97a13dac88d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bec93665f62b7a4b58db",
+    "url": "/imdb-visualizer/static/js/runtime~main.bec93665.js"
+  },
+  {
+    "revision": "e593c95176de17b63e13",
+    "url": "/imdb-visualizer/static/js/main.e593c951.chunk.js"
+  },
+  {
+    "revision": "d2e52c9534e6030dc853",
+    "url": "/imdb-visualizer/static/js/2.d2e52c95.chunk.js"
+  },
+  {
+    "revision": "e593c95176de17b63e13",
+    "url": "/imdb-visualizer/static/css/main.1e9afa8f.chunk.css"
+  },
+  {
+    "revision": "d2e52c9534e6030dc853",
+    "url": "/imdb-visualizer/static/css/2.d68354f3.chunk.css"
+  },
+  {
+    "revision": "16460d69a2897a17f07342efde234a1e",
+    "url": "/imdb-visualizer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/imdb-visualizer/precache-manifest.a320fcee7d0022e1adf8182e3e81c2a3.js"
+  "/imdb-visualizer/precache-manifest.02ebd5b1e0593e9ecc55c97a13dac88d.js"
 );
 
 workbox.clientsClaim();
