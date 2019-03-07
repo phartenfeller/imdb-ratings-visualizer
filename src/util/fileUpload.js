bfda0d7a50c032518ac3a30e313ec034a186700d
phartenfeller <pharti@hotmail.de>
2019-03-07 12:23:47 +0100

    Fixed errors in conversion from csv to json

--- src/util/fileUpload.js
+++ src/util/fileUpload.js
@@ -1,3 +1,5 @@
+const separator = '***';
+
 /**
  * Upload Process for File
  * @param {Blob} file
@@ -36,19 +38,18 @@ function readFile(file) {
  * @return {JSON}
  */
 function csvToJson(rawCsv) {
-  let csv = rawCsv.replace('Const,', 'id,');
-  csv = csv.replace('Your Rating,', 'rating,');
-  csv = csv.replace('Title,', 'title,');
+  const csv = cleanData(rawCsv);
 
   const lines = csv.split('\n');
+  console.log(lines[0]);
   const result = [];
-  const headers = lines[0].split(',');
+  const headers = lines[0].split(separator);
 
   lines.map((line, indexLine) => {
     if (indexLine < 1) return result; // Jump header line
 
     const obj = {};
-    const currentline = line.split(',');
+    const currentline = line.split(separator);
 
     headers.map((header, indexHeader) => {
       obj[header] = currentline[indexHeader];
@@ -61,7 +62,37 @@ function csvToJson(rawCsv) {
 
   result.pop(); // remove the last item because undefined values
 
+  console.log(result);
   return result;
+}
+
+/**
+ * Clean CSV Data
+ * @param {String} csv
+ * @return {String}
+ */
+function cleanData(csv) {
+  let cleanedCsv;
+  // change headers
+  cleanedCsv = csv.replace('Const,', 'id,');
+  cleanedCsv = cleanedCsv.replace('Your Rating,', 'rating,');
+  cleanedCsv = cleanedCsv.replace('Title,', 'title,');
+
+  // change all commas in titles to ###
+  cleanedCsv = cleanedCsv.replace(/"[^"]+"/g, v => {
+    return v.replace(/,/g, '###');
+  });
+
+  // change all comma seperators to own separator
+  cleanedCsv = cleanedCsv.replace(/,/g, separator);
+
+  // change back to commas in title
+  cleanedCsv = cleanedCsv.replace(/###/g, ',');
+
+  // remove quotes in titles
+  cleanedCsv = cleanedCsv.replace(/"/g, '');
+
+  return cleanedCsv;
 }
 
 export { uploadFile };
