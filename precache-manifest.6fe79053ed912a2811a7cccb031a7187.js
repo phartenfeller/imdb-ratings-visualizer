655ecfba06f6b7268e9198e5b33de4be34c93768
phartenfeller <pharti@hotmail.de>
2019-03-12 11:26:45 +0100

    Updates

--- precache-manifest.6fe79053ed912a2811a7cccb031a7187.js
+++ precache-manifest.6fe79053ed912a2811a7cccb031a7187.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "d0b78f9d40623d68574b",
+    "revision": "6ff6403812cb9c907826",
     "url": "/imdb-ratings-visualizer/static/css/main.97e59377.chunk.css"
   },
   {
-    "revision": "d0b78f9d40623d68574b",
-    "url": "/imdb-ratings-visualizer/static/js/main.d0b78f9d.chunk.js"
+    "revision": "6ff6403812cb9c907826",
+    "url": "/imdb-ratings-visualizer/static/js/main.6ff64038.chunk.js"
   },
   {
     "revision": "2d57992af04eff17e876",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/imdb-ratings-visualizer/static/js/2.194b5c55.chunk.js"
   },
   {
-    "revision": "d1cf9baf622b349dd1525814fab474dd",
+    "revision": "6b2718834b84a2d19db5528d5672bfd6",
     "url": "/imdb-ratings-visualizer/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/imdb-ratings-visualizer/precache-manifest.0b87861d10099b445ff77b52402d69ed.js"
+  "/imdb-ratings-visualizer/precache-manifest.6fe79053ed912a2811a7cccb031a7187.js"
 );
 
 workbox.clientsClaim();
