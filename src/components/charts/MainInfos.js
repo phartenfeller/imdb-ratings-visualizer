38671f7fd01a46a7b26e32bbe3dc8360665a6259
phartenfeller <pharti@hotmail.de>
2019-03-16 14:43:10 +0100

    New Categories for Main Infos

--- src/components/charts/MainInfos.js
+++ src/components/charts/MainInfos.js
@@ -4,12 +4,18 @@ import './MainInfos.scss';
 
 const MainInfos = ({ ratings, ratingsCount }) => {
   const ratingsSum = ratings.reduce((a, b) => a + (b.rating || 0), 0);
-  const average = (ratingsSum / ratingsCount).toFixed(1);
+  const average = parseFloat((ratingsSum / ratingsCount).toFixed(1));
+  const pubRatingsSum = ratings.reduce((a, b) => a + (b.imdbRating || 0), 0);
+  const pubAverage = parseFloat((pubRatingsSum / ratingsCount).toFixed(1));
+  const minutesSum = ratings.reduce((a, b) => a + (b.runtime || 0), 0);
+  const daysWatched = parseFloat((minutesSum / 60 / 24).toFixed(1));
 
   return (
     <div className="chart-container">
       <KeyValue description="ratings" value={ratingsCount} />
       <KeyValue description="your avg rating" value={average} />
+      <KeyValue description="public avg rating" value={pubAverage} />
+      <KeyValue description="days watched" value={daysWatched} />
     </div>
   );
 };
