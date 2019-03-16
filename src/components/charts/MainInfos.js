b11fe7c865e91eef1cf353925957e4f7dd4db274
phartenfeller <pharti@hotmail.de>
2019-03-16 19:41:53 +0100

    Main Info alignment and refactoring

--- src/components/charts/MainInfos.js
+++ src/components/charts/MainInfos.js
@@ -2,7 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './MainInfos.scss';
 
-const MainInfos = ({ ratings, ratingsCount }) => {
+/**
+ * Get average, public average and days watched
+ * @param {Array} ratings
+ * @param {Number} ratingsCount
+ * @return {Array} average, puAverage, daysWatched
+ */
+function getAnalyzedData(ratings, ratingsCount) {
   const ratingsSum = ratings.reduce((a, b) => a + (b.rating || 0), 0);
   const average = parseFloat((ratingsSum / ratingsCount).toFixed(1));
   const pubRatingsSum = ratings.reduce((a, b) => a + (b.imdbRating || 0), 0);
@@ -10,12 +16,25 @@ const MainInfos = ({ ratings, ratingsCount }) => {
   const minutesSum = ratings.reduce((a, b) => a + (b.runtime || 0), 0);
   const daysWatched = parseFloat((minutesSum / 60 / 24).toFixed(1));
 
+  return [average, pubAverage, daysWatched];
+}
+
+const MainInfos = ({ ratings, ratingsCount }) => {
+  const [average, pubAverage, daysWatched] = getAnalyzedData(
+    ratings,
+    ratingsCount
+  );
+
   return (
     <div className="chart-container">
-      <KeyValue description="ratings" value={ratingsCount} />
-      <KeyValue description="your avg rating" value={average} />
-      <KeyValue description="public avg rating" value={pubAverage} />
-      <KeyValue description="days watched" value={daysWatched} />
+      <table>
+        <tbody>
+          <KeyValue description="ratings" value={ratingsCount} />
+          <KeyValue description="your avg rating" value={average} />
+          <KeyValue description="public avg rating" value={pubAverage} />
+          <KeyValue description="days watched" value={daysWatched} />
+        </tbody>
+      </table>
     </div>
   );
 };
@@ -27,10 +46,10 @@ MainInfos.propTypes = {
 
 const KeyValue = ({ description, value }) => {
   return (
-    <div>
-      <span className="info-value">{value}</span>
-      <span className="info-key">{description}</span>
-    </div>
+    <tr>
+      <td className="info-value">{value}</td>
+      <td className="info-key">{description}</td>
+    </tr>
   );
 };
 
