15d3d1ddd70e06f9448c1ebf8038f94478aec76b
phartenfeller <pharti@hotmail.de>
2019-11-01 23:24:14 +0100

    Started Decade chart

--- src/components/charts/RatingsPerDecade.js
+++ src/components/charts/RatingsPerDecade.js
@@ -1,81 +1,94 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import LineColumn from './LineColumn';
+import { VictoryChart, VictoryLine, VictoryTheme, VictoryAxis } from 'victory';
 
 /**
  * Convert a year to it's decade
  * @param {Number} year
  * @return {Number}
  */
-function yearToDecade(year) {
+const yearToDecade = year => {
   return Math.floor(year / 10) * 10;
-}
+};
 
-/**
- * returns decades for x-axis
- * @return {Array}
- */
-function getDecades() {
-  // earliest movie on imdb is from 1874 https://www.imdb.com/title/tt3155794/?ref_=ttls_li_tt
-  const firstDecade = 1870;
-  const now = new Date();
-  const arr = [];
+const getDecadeIndex = (decadeArray, decade) => {
+  return decadeArray.findIndex(entries => entries.x === decade);
+};
 
-  for (let i = firstDecade; i <= yearToDecade(now.getFullYear()); i += 10) {
-    arr.push(i);
-  }
+const analyseDecadeData = ratings => {
+  const decades = ratings.map(rating => {
+    return { decade: yearToDecade(rating.year), rating: rating.rating };
+  });
 
-  return arr;
-}
+  decades.sort((a, b) => a.decade - b.decade);
 
-/**
- * Returns amount of Ratings and avg rating per decade
- * @param {Array} ratings
- * @param {Array} decades
- * @return {Array}
- */
-function analysePerDecades(ratings, decades) {
-  const amountDecades = decades.length;
-  const counts = Array(amountDecades).fill(0);
-  const avgRating = Array(amountDecades).fill(0);
+  const decadeCount = decades.reduce((prev, curr) => {
+    const index = getDecadeIndex(prev, curr.decade);
+    index !== -1 ? prev[index].y++ : prev.push({ x: curr.decade, y: 1 });
+    return prev;
+  }, []);
 
-  for (let i = 0; i < ratings.length; i++) {
-    const rating = ratings[i].rating;
-    const decade = yearToDecade(ratings[i].year);
-    const index = decades.indexOf(decade);
-    counts[index] = counts[index] + 1;
-    avgRating[index] = avgRating[index] + rating;
-  }
+  const decadeCountCopy = [...decadeCount];
 
-  for (let i = 0; i < avgRating.length; i++) {
-    avgRating[i] = avgRating[i]
-      ? parseFloat((avgRating[i] / counts[i]).toFixed(1))
-      : 0;
-  }
+  ratings.map(rating => {
+    const index = getDecadeIndex(decadeCountCopy, yearToDecade(rating.year));
 
-  console.log('counts =>', counts);
-  console.log('avgRating =>', avgRating);
+    decadeCountCopy[index].sum
+      ? (decadeCountCopy[index].sum =
+          decadeCountCopy[index].sum + rating.rating)
+      : (decadeCountCopy[index].sum = rating.rating);
 
-  return [counts, avgRating];
-}
+    return 1;
+  });
+
+  console.log('decadeCountCopy =>', decadeCountCopy);
+
+  const decadeAverage = decadeCountCopy.map(decade => {
+    return { x: decade.x, y: parseFloat(decade.sum / decade.y).toFixed(1) };
+  });
+
+  return [decadeCount, decadeAverage];
+};
 
 const RatingsPerDecade = ({ ratings }) => {
-  const decades = getDecades();
-  console.log('decades =>', decades);
-  const [counts, avgRating] = analysePerDecades(ratings, decades);
+  const data = analyseDecadeData(ratings);
+  console.log('decadeCount =>', data[0]);
+  console.log('decadeAverage =>', data[1]);
+
+  const colors = ['red', 'green'];
+  const maxima = data.map(dataset => Math.max(...dataset.map(d => d.y)));
+  // const [counts, avgRating] = analysePerDecades(ratings, decades);
 
   return (
-    <div>
-      <LineColumn
-        title="Ratings per Decade"
-        categories={decades}
-        columnName="Amount of Ratings"
-        columnData={counts}
-        lineName="Average Rating"
-        lineData={avgRating}
-        width={1200}
-        height={378}
-      />
+    <div className="card w-1/2">
+      <VictoryChart theme={VictoryTheme.material} height={350} width={500}>
+        <VictoryAxis />
+        {data.map((d, i) => (
+          <VictoryAxis
+            dependentAxis
+            key={i}
+            // offsetX={xOffsets[i]}
+            style={{
+              axis: { stroke: colors[i] },
+              // ticks: { padding: tickPadding[i] },
+              // tickLabels: { fill: colors[i], textAnchor: anchors[i] },
+            }}
+            // Use normalized tickValues (0 - 1)
+            // tickValues={[0.25, 0.5, 0.75, 1]}
+            // Re-scale ticks by multiplying by correct maxima
+            tickFormat={t => `${t}`}
+          />
+        ))}
+        {data.map((d, i) => (
+          <VictoryLine
+            key={i}
+            data={d}
+            style={{ data: { stroke: colors[i] } }}
+            // normalize data
+            y={datum => datum.y / maxima[i]}
+          />
+        ))}
+      </VictoryChart>
     </div>
   );
 };
